4c20e6217fa76ba8f67a6d3731080e03b302b279
hanegg86-svg <hanegg86@gmail.com>
2026-08-02 20:18:15 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png', // <-- เพิ่มบรรทัดนี้เข้ามา
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js'
+];
